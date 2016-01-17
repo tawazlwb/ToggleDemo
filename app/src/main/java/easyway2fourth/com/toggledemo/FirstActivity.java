97d688b455234f9606a0bbd9ec7600302ed70c20
tawazlwb <tawazlwb@gmail.com>
2016-01-17 22:49:59 +0100

    Tuto 14 : Working with Spinner

--- app/src/main/java/easyway2fourth/com/toggledemo/FirstActivity.java
+++ app/src/main/java/easyway2fourth/com/toggledemo/FirstActivity.java
@@ -8,15 +8,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class FirstActivity extends AppCompatActivity {
 
     TextView text;
     Switch switchButton;
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,23 @@ public class FirstActivity extends AppCompatActivity {
                 else{
                     text.setText("Wifi Off");
                 }
+            }
+        });
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.country_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
